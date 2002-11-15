80db926303fc3f0f61a9c37cc5e8d7c500cd14f5
alex <alex>
2002-11-15 06:40:29 +0000

    xx

---
+++ view.php
@@ -0,0 +1,58 @@
+<? require 'inc/lib.inc';
+
+$html_top = '
+
+<HTML><HEAD>
+<TITLE>File viewer</TITLE>
+<LINK rel="stylesheet" href="style.css" type="text/css">
+</HEAD><BODY bgcolor="ccdd88" background="images/BG-shadowleft.gif">
+<TABLE width="100%">
+<TR>
+ <TD align=left><P>
+ <TH align=right><A HREF="manual.html#view">Help</A></TH>
+</TR>
+</TABLE>
+<HR><P>
+';
+
+$html_bottom = '
+</BODY>
+</HTML>
+';
+
+#
+# MAIN
+#
+get_input();
+
+if ($INPUT_VARS['error'])
+    $color='RED';
+else
+    $color = 'BLACK';
+
+print $html_top;
+$file = $INPUT_VARS['file'];
+if ($INPUT_VARS['base'] == "LOGS") {
+    $base = $LOG_DIR;
+	$tbase = "Log file";
+}
+else {
+    $base = $HOST_DIR;
+	$tbase = "File";
+}
+	
+if ($file) {
+	if (preg_match('/\.\./', $file)) {
+    	print "<H3>Incorrect file name $file</H3>\n";
+	}
+
+	print "<H3>$tbase: $file</H3>\n";
+	print "<HR><PRE>\n";
+    print "<FONT COLOR=$color>\n";
+	print join("",file("$base/$file"));
+	print "</FONT\n";
+	print "</PRE>\n";
+};
+print $html_bottom;
+?>
+
